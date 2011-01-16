deeddfd1d0194cbe0863961e3aec1b8ff02f61e8
Niels Meyering <nmeyering@gmail.com>
2011-01-16 22:12:59 +0100

    another typo

--- texture3d.cpp
+++ texture3d.cpp
@@ -720,7 +720,7 @@ try
 			static_cast<sge::renderer::scalar>(
 				200.0),
 			// position
-			sge::renderer::vector3( 0.f, 2.f, 5.f ),
+			sge::renderer::vector3( -5.f, 1.f, 3.f ),
 			// Maus und Keyboard
 			*sys.keyboard_collector(),
 			*sys.mouse_collector()));
