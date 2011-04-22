216bcf97f0d87fe4357463ab4e26911bb0756c31
Niels Meyering <nmeyering@gmail.com>
2011-04-22 20:38:50 +0200

    first (kinda) working version of n-dimensional noise

--- simplex_noise.hpp
+++ simplex_noise.hpp
@@ -4,6 +4,8 @@
 #include <boost/range/algorithm_ext/iota.hpp>
 #include <boost/range/algorithm/random_shuffle.hpp>
 #include <fcppt/math/vector/vector.hpp>
+#include <fcppt/math/matrix/matrix.hpp>
+#include <cmath>
 #include <vector>
 
 namespace sgevol
@@ -28,10 +30,7 @@ public:
 	sample(
 		vector const &);
 
-	simplex_noise<
-		Float,
-		N
-	>(
+	simplex_noise(
 		std::size_t const _dim);
 
 private:
@@ -44,11 +43,140 @@ private:
 		index_type
 	> index_container;
 
-	// index_type index(); // wie?
+	typedef
+	typename
+	fcppt::math::matrix::static_<
+		Float,
+		N,
+		N
+	>::type
+	matrix;
 
 	std::size_t dim_;
 	index_container perm_;
 	std::vector<vector> gradients_;
+
+	std::size_t 
+	mod(
+	long int const &a,
+	unsigned int const &b)
+	{
+		if (a >= 0)
+			return a % b;
+		else 
+			return (-a) % b;
+	}
+
+	index_type
+	index(
+		vector const &vec)
+	{
+		index_type res = static_cast<index_type>(0);
+		for (typename vector::const_iterator it = vec.begin(); it != vec.end(); ++it)
+		{
+			long int t = static_cast<long int>(*it);
+			res = perm_.at(mod((res + t), 256u));
+		}
+		return res;
+	}
+
+	vector
+	unit(
+		typename vector::size_type const &n)
+	{
+		vector tmp = vector::null();
+		tmp[n] = static_cast<
+			typename vector::size_type>(1);
+		return tmp;
+	}
+
+	std::vector<
+		vector>
+	corners(
+		vector point)
+	{
+		std::vector<vector> res;
+		vector cur = vector::null();
+		Float max = static_cast<Float>(-1);
+		std::size_t max_i = 0;
+
+		res.push_back(vector(cur));
+
+		for (std::size_t j = 0; j < N; ++j)
+		{
+			for (std::size_t i = 0; i < N; ++i)
+				if (point[i] > max)
+				{
+					max = point[i];
+					max_i = i;
+				}
+			max = static_cast<Float>(-1);
+			point[max_i] = static_cast<Float>(-2);
+			cur += unit(max_i);
+			res.push_back(vector(cur));
+		}
+
+		return res;
+	}
+	
+	Float
+	stretch_factor()
+	{
+		return static_cast<Float>(
+			1.0/(1.0 + std::sqrt(1.0 + N))
+		);
+	}
+	
+	Float
+	inv_factor()
+	{
+		return static_cast<Float>(
+			-1.0/(1.0 + N + std::sqrt(1.0 + N))
+		);
+	}
+
+	matrix
+	stretch_m()
+	{
+		matrix tmp;
+		for (typename matrix::size_type i = 0; i < N; ++i)
+			for (typename matrix::size_type j = 0; j < N; ++j)
+				tmp[i][j] =
+					stretch_factor() +
+					((i == j)? 1 : 0 );
+		return tmp;
+	}
+
+	matrix
+	inv_m()
+	{
+		matrix tmp;
+		for (typename matrix::size_type i = 0; i < N; ++i)
+			for (typename matrix::size_type j = 0; j < N; ++j)
+				tmp[i][j] =
+					inv_factor() +
+					((i == j)? 1 : 0 );
+		return tmp;
+	}
+
+	Float
+	contrib(
+		vector const &v,
+		vector const &intv)
+	{
+		Float t = static_cast<Float>(0.6);
+		t -= fcppt::math::vector::dot(v,v);
+		// DEBUG
+		// std::cout << "called index() with: " << intv[0] << "\t" << intv[1] << std::endl;
+		// std::cout << "index() returned: " << index(intv) << std::endl;
+		if (t < 0) 
+			return static_cast<Float>(0);
+		else
+			return t * t * t * t * fcppt::math::vector::dot(
+				gradients_.at(
+					index(intv) % gradients_.size()),
+				v);
+	}
 };
 
 template<typename Float, std::size_t N>
@@ -64,7 +192,8 @@ simplex_noise<Float,N>::simplex_noise(
 	boost::random_shuffle(perm_);
 	for (std::size_t i = 0; i < N; ++i)
 	{
-		vector tmp = vector::null(); tmp[i] = 1;
+		vector tmp = vector::null();
+		tmp[i] = 1;
 		gradients_.push_back(tmp);
 		tmp[i] = -1;
 		gradients_.push_back(tmp);
@@ -76,7 +205,41 @@ Float
 simplex_noise<Float,N>::sample(
 	vector const &in)
 {
-	return static_cast<Float>(0.0);
+	Float res = static_cast<Float>(0);
+	vector tmp = stretch_m() * in;
+	BOOST_FOREACH(typename vector::reference i, tmp)
+	{
+		i = std::floor(i);
+	}
+	vector floored(tmp);
+	tmp = inv_m() * tmp;
+	tmp = in - tmp;
+	vector offset(tmp);
+	tmp = stretch_m() * tmp;
+	/*
+	BOOST_FOREACH(typename vector::reference i, tmp )
+	{
+		std::cout << i  << "\t";
+	}
+	std::cout << std::endl;
+	*/
+
+	std::vector<vector> c = corners(tmp);
+	BOOST_FOREACH(vector &v, c)
+	{
+		vector t(in - inv_m() * (floored + v));
+		/*
+		BOOST_FOREACH(typename vector::reference i, v)
+		{
+			std::cout << i << "\t";
+		}
+		std::cout << std::endl;
+		*/
+		res +=
+			contrib(t, floored + v);
+	}
+
+	return 23.f * res;
 }
 
 }
--- test.cpp
+++ test.cpp
@@ -1,54 +1,90 @@
 #include <fcppt/container/grid/object.hpp>
 #include <iostream>
 #include <cmath>
-#include "simplex2d.hpp"
-#include "simplex3d.hpp"
 #include "simplex_noise.hpp"
 #include "to_ppm.hpp"
 
 int main()
 {
-	using sgevol::simplex2d;
-	using sgevol::simplex3d;
+	std::size_t const dim = 512;
+	std::size_t const n = 4; // dimensionality - also change sampling vector below!
+	std::size_t const octaves = 5;
+	float const scaling = 4.f;
 
-	std::size_t const dim = 256;
-	simplex2d s(dim);
-  simplex3d foo(dim);
-
-	sgevol::simplex_noise<float,3> noisy(dim);
+	sgevol::simplex_noise<float,n> noisy(dim);
 
 	typedef
-	fcppt::container::grid::object<unsigned char,2>
+	fcppt::container::grid::object<
+		float,
+		2
+	>
 	grid;
 
-	grid noise(
+	typedef
+	fcppt::container::grid::object<
+		unsigned char,
+		2
+	>
+	output_grid;
+
+	typedef
+	sgevol::simplex_noise<float,n>::vector
+	vec;
+
+	grid tmpgrid(
 		grid::dim(
 			dim,
 			dim));
 
   using std::pow;
-  for (grid::size_type oct = 0; oct < 5; ++oct)
-    for (grid::size_type y = 0; y < dim; ++y)
-      for (grid::size_type x = 0; x < dim; ++x)
-      {
-        float tmp =
-          foo.sample(simplex3d::vec3(
-          static_cast<float>(x)/((static_cast<float>(dim)/pow(2,oct))),
-          static_cast<float>(y)/((static_cast<float>(dim)/pow(2,oct))),
-          0.f));
+	for (std::size_t oct = 0; oct < octaves; ++oct)
+	for (grid::size_type y = 0; y < dim; ++y)
+		for (grid::size_type x = 0; x < dim; ++x)
+		{
+			float tmp =
+				noisy.sample(
+					vec(
+						scaling * std::pow(2.f, static_cast<float>(oct)) * static_cast<float>(x)/
+							static_cast<float>(dim),
+						scaling * std::pow(2.f, static_cast<float>(oct)) * static_cast<float>(y)/
+							static_cast<float>(dim),
+						0.f,
+						0.f)); // pad to fill {3,4} dimensions
 
-        noise[
-          grid::dim(
-            x,
-            y)
-        ] += static_cast<unsigned char>( 128 + 127 * (tmp/pow(2,oct)) );
-      }
+			tmpgrid[
+				grid::dim(
+					x,
+					y)
+			] += tmp * std::pow(0.5f, static_cast<float>(oct));
+		}
 
-	std::cout << noisy.sample(simplex3d::vec3(1.f,1.f,1.f)) << std::endl;
+	output_grid output(
+		output_grid::dim(
+			dim,
+			dim));
+	
+	for (output_grid::size_type y = 0; y < dim; ++y)
+		for (output_grid::size_type x = 0; x < dim; ++x)
+		{
+			output[
+				output_grid::dim(
+					x,
+					y)
+			] = static_cast<unsigned char>(
+			256 *
+			(0.5f + 0.5f *
+			tmpgrid[
+				grid::dim(
+					x,
+					y
+				)
+			]));
+		}
 
 	to_ppm(
-		noise,
+		output,
 		std::cout,
 		static_cast<unsigned char>(255));
-	// std::cout << s.sample(simplex2d::vec2(99.f,59.f)) << std::endl;
+
+	//std::cout << noisy.sample(vec(0.5f,0.78f)) << std::endl;
 }
