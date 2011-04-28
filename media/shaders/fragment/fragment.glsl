16193592e72cca538c71061630d09f1fd1e5ed86
Niels Meyering <nmeyering@gmail.com>
2011-04-28 12:15:34 +0200

    tweaked test shader a bit

--- media/shaders/fragment/fragment.glsl
+++ media/shaders/fragment/fragment.glsl
@@ -9,7 +9,7 @@ bool
 grid_point(
 	float foo)
 {
-	return fract(10*foo) < 0.05;
+	return fract(10*foo) < 0.05 && foo > 0.05 && foo < 0.95;
 }
 
 void
@@ -17,29 +17,25 @@ main()
 {
 	vec3 p = (position_interp + 1.0) * 0.5;
 	vec3 dir = normalize(position_interp - camera);
-	int steps = 3;
+	int steps = 5;
 	float stepsize = 0.05;
 
 	vec4 acc = vec4(0.0);
 
 	for (int i = 0; i < steps; ++i)
 	{
-		if (
+		if(
 			grid_point(p.x) ||
 			grid_point(p.y) ||
-			grid_point(p.z)
-			)
+			grid_point(p.z) )
 		{
-			acc += (1.0 - acc.a) * vec4(
-					abs(p.x - 0.5),
-					abs(p.y - 0.5),
-					abs(p.z - 0.5),
-					1.0 / steps);
+		acc += vec4(
+				0.0,
+				1.0,
+				0.0,
+				1.0 / steps);
 		}
-
-		if( acc.a > 0.95 )
-			break;
-
+		
 		p += dir * stepsize;
 	}
 	frag_color = acc;
