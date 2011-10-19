35fdc33126c4a7936efe4bee461cef32c78bbc83
Niels Meyering <nmeyering@gmail.com>
2011-10-19 17:38:23 +0200

    first working shader noise

--- media/shaders/vertex/vertex.glsl
+++ media/shaders/vertex/vertex.glsl
@@ -7,10 +7,10 @@ out vec3 position_interp;
 void
 main()
 {
-	gl_Position = 
+	gl_Position =
 		mvp * vec4(position,1.0);
 
-	position_interp = 
+	position_interp =
 		//vec3(mv * vec4(position,1.0));
-    position;
+		position;
 }
